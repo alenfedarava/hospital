7dd2a254b943600d57dc2e1c1db5e8a0d507a676
alenfedarava <alena@qaprosoft.com>
2017-09-28 20:37:39 +0300

    updated relations in hospital_model

---
+++ java-project/hospital/src/main/resources/INSERT.sql
@@ -0,0 +1,13 @@
+USE hospital;
+INSERT INTO Address (houseNumber,phone,street,cities) VALUES 
+('5', '+375298916671','Lenina','MINSK'),
+('6', '+375298916672','Komsomolskaya','BORISOV'),
+('7', '+375298916673','Minina','CHERVEN'),
+('8', '+375298916674','Frunze','FANIPOL'),
+('9', '+375298916675','Mayakovskaya','DZERZHINSK'),
+('10', '+375298916676','Solnechnaya','KLETSK'),
+('11', '+375298916677','Fabrichnaya','KOPYL');
+
+INSERT INTO ChiefMedical (phD,awards) VALUES ('1','2'), ('0','1');
+
+INSERT INTO ChiefAccountant (economicEducation) VALUES ('1'), ('0');
